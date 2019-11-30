6341e4e12bff3dd62978b084d453b20d10bcf4e8
johnkrovitch <arnaudfrezet@gmail.com>
2019-11-30 10:14:08 +0100

    Add a compiler pass to test private services

--- tests/MediaBundle/Kernel/TestKernel.php
+++ tests/MediaBundle/Kernel/TestKernel.php
@@ -4,25 +4,33 @@ namespace JK\MediaBundle\Tests\Kernel;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use JK\MediaBundle\JKMediaBundle;
+use JK\MediaBundle\Tests\DependencyInjection\PublicServicePass;
 use Oneup\UploaderBundle\OneupUploaderBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
 class TestKernel extends Kernel
 {
+    protected function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new PublicServicePass());
+    }
+
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             // Dependencies
             new FrameworkBundle(),
+
             new DoctrineBundle(),
             new TwigBundle(),
             // My Bundle to test
             new JKMediaBundle(),
             new OneupUploaderBundle(),
-        );
+        ];
 
         return $bundles;
     }
