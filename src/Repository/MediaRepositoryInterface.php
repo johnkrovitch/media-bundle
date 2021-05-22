9a12cc79b72d6d4c3ade18567644a8ef027e1698
Arnaud Frézet <arnaudfrezet@gmail.com>
2021-05-22 14:35:40 +0200

    Update dependencis for php 8.0 and Symfony 5.2

--- src/Repository/MediaRepositoryInterface.php
+++ src/Repository/MediaRepositoryInterface.php
@@ -4,11 +4,12 @@ namespace JK\MediaBundle\Repository;
 
 use Doctrine\Persistence\ObjectRepository;
 use JK\MediaBundle\Entity\MediaInterface;
-use JK\Repository\RepositoryInterface;
 use Pagerfanta\Pagerfanta;
 
-interface MediaRepositoryInterface extends RepositoryInterface, ObjectRepository
+interface MediaRepositoryInterface extends ObjectRepository
 {
+    public function add(MediaInterface $media): void;
+
     public function create(): MediaInterface;
 
     public function paginate($page = 1, $maxPerPage = 9): Pagerfanta;
