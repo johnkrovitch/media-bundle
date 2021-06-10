732584245ca84216b17fc361fd2c2fb74125600f
Arnaud Frézet <arnaudfrezet@gmail.com>
2021-06-10 19:36:53 +0200

    test: add a kernel test case for phpunit booting a real kernel

---
+++ tests/phpunit/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JK\MediaBundle\Tests;
+
+use JK\MediaBundle\Tests\Kernel\TestKernel;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    protected function createKernel(): KernelInterface
+    {
+        $kernel = new TestKernel('test', true);
+        $kernel->boot();
+
+        return $kernel;
+    }
+
+    /**
+     * Assert that the given service class is configured in the services.yaml.
+     */
+    protected function assertServiceExists(string $serviceId): void
+    {
+        $kernel = $this->createKernel();
+        $container = $kernel->getContainer();
+
+        $this->assertTrue($container->has($serviceId));
+    }
+}
