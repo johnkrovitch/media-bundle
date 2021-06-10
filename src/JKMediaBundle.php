da8e7721a69daf63e1b562f4bf2af851b4f2707c
Arnaud Frézet <arnaudfrezet@gmail.com>
2021-06-10 21:01:46 +0200

    feat: update the dependency injection and bundle configuration

--- src/JKMediaBundle.php
+++ src/JKMediaBundle.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace JK\MediaBundle;
 
+use JK\MediaBundle\DependencyInjection\JKMediaExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JKMediaBundle extends Bundle
 {
+    protected function createContainerExtension(): JKMediaExtension
+    {
+        return new JKMediaExtension();
+    }
 }
